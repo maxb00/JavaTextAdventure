23a6a71a8a8bd93787d5cdaa05735af51fab9cf4
greenisfun100 <alex.hiser@gmail.com>
2015-07-01 11:18:00 -0400

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Made by Liam Pierce and Max Barlow.");
+		System.out.println("Made by Liam Pierce, Max Barlow, and Alex Hiser");
 		
 	}
 
