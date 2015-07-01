bf8658b12acf1739a9cf81e789a5d8c1b40397d0
maxb00 <maxbar00@yahoo.com>
2015-07-01 11:26:42 -0400

    Started setup

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Made by Liam Pierce, Max Barlow, and Alex Hiser");
+		print("Made by Liam Pierce and Alex Hiser (Code), and Max Barlow (Story).")
+		RoomHandler RH = new RoomHandler();
+		boolean gamerunning = true;
 		
+		while (gamerunning){
+		//	RH.room()
+			
+		}
+		
+	}
+	
+	public void print(String print){
+		System.out.println(print);
 	}
 
 }
