5843fd521a69bc479d64b89fecdc080be4146c8c
DeadMansMarch <lsp425@gmail.com>
2015-07-01 11:32:00 -0400

    Update RoomHandler.java

--- src/main/rooms/RoomHandler.java
+++ src/main/rooms/RoomHandler.java
@@ -1,1 +1,13 @@
 
+public class RoomHandler{
+  static int roomnumbers = 0;
+  public RoomHandler(String Roomname,int Roomnumber,Object event){
+    roomnumbers = roomnumbers + 1;
+    
+  }
+  
+  public void room(Object event){
+    int roomending = event();
+    
+  }
+}
